4343f8dfa028187a39a22e8c91558815b3dc911a
Justina-R <justina.rey2602@gmail.com>
2026-01-30 12:42:35 -0300

    Add config for Docker in Program.cs

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -36,4 +36,8 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+// Added for Docker config
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+app.Urls.Add($"http://0.0.0.0:{port}");
+
 app.Run();
